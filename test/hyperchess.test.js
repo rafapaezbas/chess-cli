1c4f75c1a2daf0c97474278c54127ddad27674c2
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-11-09 09:33:22 +0000

    add hyperchess mate test

--- test/hyperchess.test.js
+++ test/hyperchess.test.js
@@ -14,22 +14,65 @@ test('move', async (t) => {
   t.ok(commit)
 })
 
-test('remote move', async (t) => {
+test('remote move', async t => {
   const a = keyPair()
   const b = keyPair()
 
   const white = new HyperChess(a, b.publicKey)
-  await white.ready()
-
   const black = new HyperChess(b, a.publicKey)
+
+  await white.ready()
   await black.ready()
 
   const e4 = { src: 12, dst: 28 }
   const commit = white.move(e4)
 
-  const remoteCommit = black.remoteMove(commit)
+  const remoteCommit = await black.remoteMove(commit)
   const position = white.chess.getPosition(true)
 
   white.state.auth.addSignature(remoteCommit)
   t.ok(white.state.core.append(position))
+})
+
+test('scholars mate', async t => {
+  const a = keyPair()
+  const b = keyPair()
+
+  const white = new HyperChess(a, b.publicKey)
+  const black = new HyperChess(b, a.publicKey)
+
+  await white.ready()
+  await black.ready()
+
+  let commit
+  commit = white.move({ src: 12, dst: 28 }) // e4 e5
+  await black.remoteMove(commit)
+
+  commit = black.move({ src: 52, dst: 36 })
+  await white.remoteMove(commit)
+
+  commit = white.move({ src: 3, dst: 39 }) // Qh5 Nc6
+  await black.remoteMove(commit)
+
+  commit = black.move({ src: 57, dst: 42 })
+  await white.remoteMove(commit)
+
+  commit = white.move({ src: 5, dst: 26 }) // Bc4 Nf6??
+  await black.remoteMove(commit)
+
+  commit = black.move({ src: 62, dst: 45 })
+  await white.remoteMove(commit)
+
+  commit = white.move({ src: 39, dst: 53 }) // Qxf7#
+  commit = await black.remoteMove(commit)
+
+  white.state.auth.addSignature(commit)
+  await white.state.core.append(white.chess.getPosition(true))
+
+  t.ok(black.chess.inCheck())
+  t.is(black.chess.availableMoves().length, 0)
+  t.is(black.chess.status(), 'WHITEWON')
+
+  t.is(black.state.core.length, white.state.core.length)
+  t.is(black.state.core.length, 7)
 })
